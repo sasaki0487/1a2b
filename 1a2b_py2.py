67b78d4f5d67a6afc98b148a25a7d23cd25fa756
ESD17 <esd17@ESD17de-iMac.local>
2019-01-11 13:51:08 +0800

    add comment

--- 1a2b_py2.py
+++ 1a2b_py2.py
@@ -10,16 +10,20 @@ frame.pack()
 but_frame = Tkinter.Frame(top)
 but_frame.pack()
 
+#時間物件
 time_str = Tkinter.StringVar()
 time_str.set("Time : 0 s")
 time_label = Tkinter.Label(frame, textvariable=time_str)
 time_label.grid(row=0, column=0)
+#輸入答案物件
 enter_box = Tkinter.Entry(frame)
 enter_box.grid(row=1, column=0)
 guess_button = Tkinter.Button(frame, text="Guess!", borderwidth=5, width=10, command=lambda: guess())
 guess_button.grid(row=2, column=0)
+#顯示結果物件
 result_box = Tkinter.Text(frame, height=20)
 result_box.grid(row=3, column=0)
+#顯示答案 重新開始 離開按鈕
 answer_button = Tkinter.Button(but_frame, text="Answer", borderwidth=5, width=10, command=lambda: show_ans())
 answer_button.grid(row=0, column=0)
 restart_button = Tkinter.Button(but_frame, text="New Game", borderwidth=5, width=10, command=lambda: restart())
@@ -27,6 +31,7 @@ restart_button.grid(row=0, column=1)
 exit_button = Tkinter.Button(but_frame, text="Exit", borderwidth=5, width=10, command=lambda: exit())
 exit_button.grid(row=0, column=2)
 
+#初始化各種東西
 round = 0
 ans = ""
 ans_list = []
@@ -34,47 +39,58 @@ name_list = []
 round_list = []
 time_list = []
 
-
+#加時間的function
 def add_time():
     global t
+    #把時間取出來
     time = int(''.join(filter(str.isdigit, time_str.get())))
+    #時間加+1
     time += 1;
+    #時間塞回去
     time_str.set("Time: " + str(time) + " s")
+    #要重複call才會一直執行
     t = Timer(1.0, add_time)
     t.start()
 
-
+#生成題目的function
 def gen_ran():
     global ans
+    #gen 4 number by sampling
     temp = random.sample(range(0, 10), 4)
+    #串起來
     ans = "".join(map(str, temp))
 
-
+#顯示答案的function
 def show_ans():
     global ans
+    #用messagebox顯示答案
     tkMessageBox.showinfo("ANS", ans)
 
-
+#贏了的時候執行的function (part1 輸入名字)
 def win():
     global toplv
     global name_box
+    #開一個新的框框
     toplv = Tkinter.Toplevel()
     frame = Tkinter.Frame(toplv)
     frame.pack()
+    #OK的按鍵 (輸入完名字後按的)
     ok_button = Tkinter.Button(frame, text="OK", borderwidth=5, width=10, command=lambda: win2())
     ok_button.grid(row=1, column=0)
     name_box = Tkinter.Entry(frame)
     name_box.grid(row=0, column=0)
     name_box.insert(0, "Enter Your Name")
 
-
+#贏了之後輸入完名字執行的function (part2 顯示高分榜)
 def win2():
     global toplv
+    #把名字存起來，把輸入名字的框框殺掉
     name = name_box.get()
     toplv.destroy()
     toplv = Tkinter.Toplevel()
     frame = Tkinter.Frame(toplv)
     frame.pack()
+    #顯示名字的文字框
     score_box = Tkinter.Text(frame, height=20)
     score_box.grid(row=0, column=0)
     score_box.insert(Tkinter.END, "Name\t\tRound\t\tTime\n")
@@ -82,25 +98,30 @@ def win2():
     restart_button.grid(row=1, column=0)
     exit_button = Tkinter.Button(frame, text="Exit", borderwidth=5, width=10, command=lambda: exit())
     exit_button.grid(row=2, column=0)
+    #把時間記起來
     time = int(''.join(filter(str.isdigit, time_str.get())))
+    #計算新的高分要插入的位置，先比回合數再比花費時間
     index = 0
     while index < len(name_list) and round > round_list[index]:
         index += 1
     while index < len(name_list) and time > time_list[index] and round == round_list[index]:
         index += 1
+    #把資訊都插入他該在的位置
     name_list.insert(index, name)
     round_list.insert(index, round)
     time_list.insert(index, time)
-    index = 0
-    for index in range(0, len(name_list)):
-        score_box.insert(Tkinter.END,
-                         name_list[index] + "\t\t" + str(round_list[index]) + "\t\t" + str(time_list[index]) + "\n")
+    #如果超過十個人上榜就刪掉多的
     if len(name_list) > 10:
         name_list.pop()
         round_list.pop()
         time_list.pop()
+    index = 0
+    #串到高分榜的文字框
+    for index in range(0, len(name_list)):
+        score_box.insert(Tkinter.END,
+                         name_list[index] + "\t\t" + str(round_list[index]) + "\t\t" + str(time_list[index]) + "\n")
 
-
+#輸掉的時候呼叫的function
 def lose():
     global toplv
     toplv = Tkinter.Toplevel()
